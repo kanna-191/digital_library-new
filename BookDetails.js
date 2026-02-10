c4d46fc653c74cbed690ebffe504a8d93814b1b6
kanna-191 <25B21IT012@adityauniversity.in>
2026-02-10 22:11:42 +0530

    Create BookDetails.js

---
+++ BookDetails.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function BookDetails() {
+  return (
+    <div>
+      <h2>Book Details</h2>
+      <p>Title:</p>
+      <p>Author:</p>
+      <p>Category:</p>
+    </div>
+  );
+}
+
+export default BookDetails;
