92bdad951e6e1370154203f32bda1f44fa334f3f
kanna-191 <25B21IT012@adityauniversity.in>
2026-02-10 22:05:47 +0530

    Create Login.js

---
+++ Login.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { loginUser } from "../services/authService";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    const res = await loginUser({ email, password });
+    localStorage.setItem("token", res.token);
+    localStorage.setItem("role", res.user.role);
+    navigate("/dashboard");
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password"
+        onChange={e => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
