80ec23225ebd3dda07679c615ef67c3537223eca
kanna-191 <25B21IT012@adityauniversity.in>
2026-02-10 22:13:39 +0530

    Create Profile.js

---
+++ Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Name:</p>
+      <p>Email:</p>
+    </div>
+  );
+}
+
+export default Profile;
